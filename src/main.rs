0b0aa5752f676add17c77177bf62a1ccfd1017a4
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-08-08 16:14:27 +0200

    Working print_paths_to_files_with_extension()

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,22 @@
 use std::path::Path;
 use std::{ffi::OsStr, fs, io};
 
-fn visit_dirs(path: &Path, cb: &dyn Fn(&Path) -> io::Result<()>) -> io::Result<()> {
-    cb(path)?;
-
+fn print_paths_to_files_with_extension(path: &Path, ext: Option<&OsStr>) -> io::Result<()> {
     for entry in fs::read_dir(path)? {
-        let entry = entry?;
-        let path = entry.path();
+        let path = entry?.path();
         if path.is_dir() {
-            visit_dirs(&path, cb)?;
+            print_paths_to_files_with_extension(&path, ext)?;
+        } else if path.extension() == ext {
+            println!("Path: {:?}", path);
         }
     }
     Ok(())
 }
 
-fn print_dir_contents(path: &Path) -> io::Result<()> {
-    let mut paths: Vec<_> = fs::read_dir(path)?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()?;
-
-    paths.sort();
-
-    println!("Folder name: {:?}, its contents:", path);
-    println!("{:?}\n\n", paths);
-
-    Ok(())
-}
-
 fn main() -> io::Result<()> {
-    let path: &Path = Path::new("./test_folder");
-    visit_dirs(path, &print_dir_contents)?;
+    let path: &Path = Path::new(".");
+    let extension = Some(OsStr::new("sgf"));
+    print_paths_to_files_with_extension(path, extension)?;
 
     Ok(())
 }
